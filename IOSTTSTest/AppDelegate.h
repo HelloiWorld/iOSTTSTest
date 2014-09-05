a5e750d7bd8051e8f67d54355749ed909b6efc66
PY <py@pytekimac-mini.cisco>
2014-09-05 11:20:30 +0800

    Initial Commit

---
+++ IOSTTSTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSTTSTest
+//
+//  Created by PY on 14-9-5.
+//  Copyright (c) 2014年 Naturalsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOSTTSTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IOSTTSTest
+//
+//  Created by PY on 14-9-5.
+//  Copyright (c) 2014年 Naturalsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
